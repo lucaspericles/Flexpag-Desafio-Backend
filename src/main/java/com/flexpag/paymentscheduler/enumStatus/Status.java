89720fe15b86d39b0608a0a98eaa10dfdc4dab95
lucaspericles <lucaspericles.fp@gmail.com>
2022-10-16 18:23:43 -0300

    add status

---
+++ src/main/java/com/flexpag/paymentscheduler/enumStatus/Status.java
@@ -0,0 +1,10 @@
+package com.flexpag.paymentscheduler.enumStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public enum Status {
+    pending,
+    paid
+}
